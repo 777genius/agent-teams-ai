b1c2bdcbde3520bf06a810c7a6e0443ef778c62e
777genius <quantjumppro@gmail.com>
2026-07-23 10:14:49 +0300

    test(change-review): characterize presentation rules

---
+++ src/features/change-review/renderer/index.ts
@@ -0,0 +1,24 @@
+export type {
+  ChangeReviewChangeSet,
+  GlobalDiffLoadingState,
+  ReviewChangeStats,
+  ReviewStats,
+  TaskChangesEmptyStatePresentation,
+} from './view-models/changeReviewPresentation';
+export {
+  buildChangeReviewTitle,
+  buildGlobalDiffLoadingState,
+  buildReviewChangeStats,
+  buildReviewFileLabels,
+  buildReviewStats,
+  buildTaskChangesEmptyStatePresentation,
+  buildWatchedReviewFilePathsKey,
+  canAcceptAllReviewFiles,
+  findActiveReviewFile,
+  getRejectablePendingReviewFiles,
+  isTaskChangeSetV2,
+  resolveReviewFileLabel,
+  shouldShowTaskScopeBanner,
+  sortChangeReviewFiles,
+  toTaskChangeSetV2,
+} from './view-models/changeReviewPresentation';
