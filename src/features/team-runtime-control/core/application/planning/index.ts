dbb1921d7ba431f2dd6efddd03bd8c137677da23
iliya <iliyazelenkog@gmail.com>
2026-09-13 11:42:41 +0000

    feat(team-runtime-control): add hosted owner launch projector
    Refs #252

--- src/features/team-runtime-control/core/application/planning/index.ts
+++ src/features/team-runtime-control/core/application/planning/index.ts
@@ -1,4 +1,5 @@
 export * from './createCompositeRuntimePlan';
 export * from './CreateRuntimePlanFromPersistedRoster';
 export * from './IssueRuntimePlanAttestation';
+export * from './projectHostedOwnerLaunchPlan';
 export type * from './RuntimePlanAttestationAuthority';
